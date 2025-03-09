4e76dd154267d88846f2de614c6a8713d461bfff
hemant9808 <143936287+Hemant9808@users.noreply.github.com>
2025-03-09 22:59:14 +0530

    add dotenv

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const app = require('./app');
 const { initializeSocket } = require('./socket');
+const  dotenv = require('dotenv');
+
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
